8a45ed47e14794123464b98ef0608193285271d1
Haoran <2559837459@qq.com>
2024-10-28 09:53:36 +0000

    first test

--- test_times.py
+++ test_times.py
@@ -7,3 +7,14 @@ def test_generic_case():
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     expected = [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
     assert compute_overlap_time(large, short) == expected
+
+def test_given_input():
+    ... 
+    result = ... 
+    expected = ...
+    assert result == expected
+
+if __name__ == "__main__":
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    print(compute_overlap_time(large, short))
